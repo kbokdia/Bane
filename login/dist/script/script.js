b0ec9c7d75f304b50c9d8fa526e6140c8c92bf2a
darshan <darshanturakhia23@gmail.com>
2016-01-16 16:27:44 +0530

    - Index Page - Login Page

--- login/dist/script/script.js
+++ login/dist/script/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+    $('select').material_select();
 });
--- login/index.php
+++ login/index.php
@@ -3,166 +3,86 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <title>Parallax Template - Materialize</title>
+    <title>Club Apps - Login</title>
 
     <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">-->
     <link href="../dist/css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="../dist/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="dist/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-<div class="navbar-fixed">
-    <nav class="white" role="navigation">
-        <div class="nav-wrapper container">
-            <a id="logo-container" href="#" class="brand-logo">Test</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="login.html">Login</a></li>
-            </ul>
-
-            <ul id="nav-mobile" class="side-nav">
-                <li><a href="login.html">Login</a></li>
-            </ul>
-            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-        </div>
-    </nav>
-</div>
-
-<div id="index-banner" class="parallax-container">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <br><br>
-            <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-            <div class="row center">
-                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
-            </div>
-            <br><br>
-
-        </div>
-    </div>
-    <div class="parallax"><img src="../images/background/background1.jpg" alt="Unsplashed background img 1"></div>
-</div>
-
-
-<div class="container">
-    <div class="section">
-
-        <!--   Icon Section   -->
-        <div class="row">
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-                    <h5 class="center">Speeds up development</h5>
-
-                    <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
-                </div>
-            </div>
-
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-                    <h5 class="center">User Experience Focused</h5>
-
-                    <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
-                </div>
-            </div>
-
-            <div class="col s12 m4">
-                <div class="icon-block">
-                    <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-                    <h5 class="center">Easy to work with</h5>
-
-                    <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="../images/background/background2.jpg" alt="Unsplashed background img 2"></div>
-</div>
-
-<div class="container">
-    <div class="section">
-
-        <div class="row">
-            <div class="col s12 center">
-                <h3><i class="mdi-content-send brown-text"></i></h3>
-                <h4>Contact Us</h4>
-                <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
-            </div>
-        </div>
-
-    </div>
-</div>
-
-
-<div class="parallax-container valign-wrapper">
-    <div class="section no-pad-bot">
-        <div class="container">
-            <div class="row center">
-                <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
-            </div>
-        </div>
-    </div>
-    <div class="parallax"><img src="../images/background/background3.jpg" alt="Unsplashed background img 3"></div>
-</div>
-
-<footer class="page-footer teal">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h5 class="white-text">Company Bio</h5>
-                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
-
-            </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Settings</h5>
-                <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+    <div class="navbar-fixed">
+        <nav class="indigo darken-3" role="navigation">
+            <div class="nav-wrapper container">
+                <a id="logo-container" href="../" class="brand-logo white-text">Club Apps</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="login.html" class="white-text">Login</a></li>
                 </ul>
-            </div>
-            <div class="col l3 s12">
-                <h5 class="white-text">Connect</h5>
-                <ul>
-                    <li><a class="white-text" href="#!">Link 1</a></li>
-                    <li><a class="white-text" href="#!">Link 2</a></li>
-                    <li><a class="white-text" href="#!">Link 3</a></li>
-                    <li><a class="white-text" href="#!">Link 4</a></li>
+
+                <ul id="nav-mobile" class="side-nav">
+                    <li><a href="login.html">Login</a></li>
                 </ul>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
             </div>
-        </div>
+        </nav>
     </div>
-    <div class="footer-copyright">
-        <div class="container">
-            Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-        </div>
+    <div class="row card-top-padding">
+        <form id="loginForm" name="loginForm" method="post">
+            <div class="col s12 m12 l4 offset-l4">
+                <div class="card-panel white">
+                    <div class="row center-align">
+                        <h5>Login</h5>
+                    </div>
+                    <div class="row">
+                        <div class="input-field">
+                            <input id="userName" placeholder="Username" name="userName" type="text" class="validate">
+                            <label for="userName">Username</label>
+                        </div>
+                        <div class="error"></div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field">
+                            <input id="password" placeholder="Password" name="password" type="password" class="validate">
+                            <label for="password">Password</label>
+                        </div>
+                        <div class="error"></div>
+                    </div>
+                    <div class="row">
+                        <div class="input-field">
+                            <select class="icons" name="clubID" id="clubID">
+                                <option value="" disabled selected>Choose your club</option>
+                                <option value="ega" data-icon="../images/clubLogos/EGA.png" class="left circle">EGA -
+                                    Evergreen Association
+                                </option>
+                                <option value="rya" data-icon="../images/clubLogos/RYA.png" class="left circle">RYA -
+                                    Rajasthan Youth Association
+                                </option>
+                                <option value="saans" data-icon="../images/clubLogos/Saans.png" class="left circle">Saans
+                                </option>
+                            </select>
+                            <label>Choose Your Club</label>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <p>
+                            <input type="checkbox" id="rememberMe"/>
+                            <label for="rememberMe" class="indigo-text">Remember Me</label>
+                        </p>
+                    </div>
+                    <div class="row">
+                        <button type="submit" class="waves-effect waves-light btn-large btn-full pink accent-3">Submit
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-</footer>
 
-
-<!--  Scripts-->
-<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="../dist/script/materialize.min.js"></script>
-<script src="../dist/script/script.js"></script>
-<script src="dist/script/script.js"></script>
-
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="../dist/script/materialize.min.js"></script>
+    <script src="../dist/script/script.js"></script>
+    <script src="dist/script/script.js"></script>
 </body>
 </html>
